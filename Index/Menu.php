92d0ac6b1ef30a3d85646bcad7101e1994d51552
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-18 20:51:31 -0300

    part1

--- Index/Menu.php
+++ Index/Menu.php
@@ -16,11 +16,28 @@
         </div>
         <div id="Tex_But">
             <h1>Menu Inicial</h1>
+            <h2>Bem-Vindo(a) <?php 
+            session_start();
+            include '../php/conexao.php';
+            $id = $_SESSION['id'];
+            $pedido = $conec->query("select * from tb_user where id_usuario='$id'");
+            $resultado = $pedido->fetch_assoc();
+            $nome = $resultado['nome'];
+        
+            if(isset($_SESSION['id'])){
+                echo $nome;
+
+            } else {
+                header("Location: ../Index/index.html");
+            }
+            ?>    
+        </h2>
             <p>
                 Cadastre aqui seus Lançamentos ou crie as categorias necessaria para eles
             </p>
             <a href="Lancamento/index.html"><button id="L">Lançamento</button></a>
             <a href="Categoria/index.html"><button id="C">Categoria</button></a>
+            <a href="Logout.php"><button id="S">Sair</button></a>
         </div>
     </div>
 </body>
--- php/loginIU.php
+++ php/loginIU.php
@@ -5,8 +5,8 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 
-$pedido = $conec->query("select email,senha from tb_user where email='$email'");
-echo "a";
+$pedido = $conec->query("select * from tb_user where email='$email'");
+
 
 if ($pedido == null) {
     header("localtion: index.html");
@@ -18,7 +18,9 @@ if ($pedido == null) {
     $senha_banco = $resultado['senha'];
 
     if ($email == $email_banco && $senha == $senha_banco) {
-        header("location: ../Index/Menu.html");
+        session_start();
+        $_SESSION['id'] = $resultado['id_usuario'];
+        header("location: ../Index/Menu.php");
     } else {
         header("Location: ../Index/index.html");
     }
